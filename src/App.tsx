5bbaf19c42b419a1dead3db2f5712b58a79e7bf7
Claudio Barsante <clbmribas@gmail.com>
2025-01-14 18:41:15 -0300

    ---clean up---

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-//import './App.css'
+
 interface Transaction {
   id: number;
   amount: number;
 }
 
 function PaymentDashboard() {
-
-
   const [transactions, setTransactions] = useState<Transaction[]>([
     { id: 1, amount: 50 },
     { id: 2, amount: 150 },
@@ -19,28 +15,26 @@ function PaymentDashboard() {
   const [result, setResult] = useState<string>('');
 
   const handleCheckTransactions = () => {
-    if (target === null) return;  
-    
+    if (target === null) return;
+
     const transactionMap = new Map<number, Transaction>();
 
-  for (const transaction of transactions) {
-    const complement = target - transaction.amount;
+    for (const transaction of transactions) {
+      const complement = target - transaction.amount;
 
-    if (transactionMap.has(complement)) {
-      const matchedTransaction = transactionMap.get(complement);
-      if (!matchedTransaction) return;
-      setResult(`Transactions ${matchedTransaction.id} and ${transaction.id} add up to ${target}`);
-      return;
+      if (transactionMap.has(complement)) {
+        const matchedTransaction = transactionMap.get(complement);
+        if (!matchedTransaction) return;
+        setResult(`Transactions ${matchedTransaction.id} and ${transaction.id} add up to ${target}`);
+        return;
+      }
+
+      transactionMap.set(transaction.amount, transaction);
     }
 
-    transactionMap.set(transaction.amount, transaction);
-  }
-
-  setResult('No matching transactions found.');
-  const id = transactions.length + 1;  
-  handleAddTransaction(id,target);
-
-   
+    setResult('No matching transactions found.');
+    const id = transactions.length + 1;
+    handleAddTransaction(id, target);
   };
 
   const handleAddTransaction = (id: number, amount: number) => {
